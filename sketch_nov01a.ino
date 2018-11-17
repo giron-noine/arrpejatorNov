6a5f5d4dd752c37831b18d9bb348d36366243682
giron-noine <42906002+giron-noine@users.noreply.github.com>
2018-11-17 18:12:35 +0900

    Add files via upload

--- sketch_nov01a.ino
+++ sketch_nov01a.ino
@@ -1,32 +1,25 @@
-#include <MozziGuts.h>
-#include <Oscil.h>
-#include <EventDelay.h>
-#include <mozzi_midi.h>
-#include <mozzi_rand.h>
-#include <Ead.h>
-#include <tables/sin2048_int8.h> // sine table for oscillator
-Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+int const nob[3] = {A0,A1,A2};
 
-int const nob[3] = {A5,A4,A3};
-
-#define switch1 7
-#define switch2 10
-
+#define switch1 10 //page
+#define switch2 2 //arrp ON
 #define looptop 12
 
-#define CONTROL_RATE 64
+#define octUP 4
+#define octDOWN 5
+int octv = 0;
 
-EventDelay kDelay;
-char pageState0;
-char pageState1;
-int valNob[2][3];
+int valNob[2][3]; //ページ番号,ノブ番号
+
+int pageState0 = 0;
+int pageState1 = 0;
+
 int stepNum = 16;
 int pattern = 1;
 int tmp_bpm = 250;
 
 int scaleNum;
 int dechune;
-int keyshift;
+int keyshift = 0;
 
 int bang = 0;
 
@@ -39,76 +32,124 @@ int scaleMap[6][7] = {
   {60, 61, 64, 65, 67, 68, 71} //gypsy
 };
 
+int noteNum[13] = {60,61,62,63,64,65,66,67,68,69,70,71,72};
+int notefreq[13] = {261,277,293,311,329,349,370,392,415,440,466,493,523};
 
+int pushkey(int);
 
-void setup(){
-  startMozzi(CONTROL_RATE);
-  kDelay.start(1000);
+void setup()
+{
   pinMode(switch1, INPUT);
+  pinMode(switch2, INPUT);
   pinMode(looptop, OUTPUT);
-  //pinMode(switch2 INPUT);
-  randSeed();
+  pinMode(octUP, INPUT);
+  pinMode(octDOWN, INPUT);
+  Serial.begin(9600);
 }
 
-void updateControl(){
-
+void loop()
+{
   if(digitalRead(switch1) == LOW){
     pageState0 = 1;
   }else{
     pageState1 = 1;
   }
-
+//page1 prossece
   if(pageState0 == 1){
     for(int i=0; i<3; i++){
-      if(valNob[1][i] != analogRead(nob[i])){
-        valNob[0][i] = analogRead(nob[i]);
+    	if(valNob[1][i] != analogRead(nob[i])){
+    		valNob[0][i] = analogRead(nob[i]);
         }
     }
     pageState0 = 0;
-  }
-  
-  if(pageState1 == 1){ 
+  }  
+//page2 prossece
+  if(pageState1 == 1){
+    
     for(int i=0; i<3; i++){
-      if(valNob[0][i] != analogRead(nob[i])){
-        valNob[1][i] = analogRead(nob[i]);
-      }
+    	if(valNob[0][i] != analogRead(nob[i])){
+    		valNob[1][i] = analogRead(nob[i]);
+    	}
     }
     pageState1 = 0;
   }
-
-  tmp_bpm = map(valNob[0][0], 0, 1023, 10, 2000);
-  stepNum = map(valNob[0][1], 0, 1023, 0, 15);
-  pattern =map(valNob[0][2], 0, 1023, 0, 5);
-  scaleNum = map(valNob[1][0], 0, 1023, 0, 5);
-  dechune = map(valNob[1][1], 0, 1023, 0, 11);
-  keyshift = map(valNob[1][2], 0, 1023, -2, 2) * 12;
-
-  //Play Sound
-  if(kDelay.ready()){
-    if(bang < stepNum){
-    switch(bang){
-      case 0:
-        digitalWrite(looptop,HIGH);
-        break;
-      default:
-        digitalWrite(looptop,LOW);
-        break;
-    }
-    aSin.setFreq(mtof(scaleMap[scaleNum][bang + dechune + keyshift])); //scaleMapの２次元配列から取り出してmtofで周波数へ
-    kDelay.start(tmp_bpm);
-    bang++;
-  }else {
-    bang = 0;
+  
+//octave control
+  if((digitalRead(octUP) == HIGH)&&(octv<3)){
+    octv++;
+    keyshift = octv*12;
+  }
+  if((digitalRead(octDOWN) == HIGH)&&(octv>-3)){
+    octv--;
+    keyshift = octv*12;
   }
   
+//push da oscillate
+  if(digitalRead(switch2) == LOW){
+    if(analogRead(A5) != 1023){
+  	tone(9,noteNum[pushkey(analogRead(A5))]);
+    }else{
+      noTone(9);
+    }
+  }else{ //Arrp mode:sometime make liblary
+    
+  }
+  
+  Serial.print("voltage");
+  Serial.println(pushkey(analogRead(A5)));
+  Serial.print("octaveU");
+  Serial.println(digitalRead(octUP));
+  Serial.print("octaveD");
+  Serial.println(digitalRead(octDOWN));
+  Serial.print("octave");
+  Serial.println(keyshift);
+  Serial.print("dial1");
+  Serial.println(analogRead(A0));
+  delay(500);
 }
 
+int pushkey(int pushSwitch){
+  if(pushSwitch < 50){
+    return 0; //0
+  }else if(pushSwitch < 100){
+    return 1; //78
+  }else if(pushSwitch < 200){
+    return 2; //158
+  }else if(pushSwitch < 300){
+    return 3; //237
+  }else if(pushSwitch < 350){
+    return 4; //318
+  }else if(pushSwitch < 420){
+    return 5; //404
+  }else if(pushSwitch < 500){
+  	return 6; //477
+  }else if(pushSwitch < 600){
+	return 7; //552
+  }else if(pushSwitch < 700){
+ 	return 8; //636
+  }else if(pushSwitch < 740){
+    return 9; //715
+  }else if(pushSwitch < 840){
+    return 10; //797
+  }else if(pushSwitch < 900){
+    return 11; //871
+  }else if(pushSwitch < 1000){
+    return 12; //947
+  }
 }
 
-int updateAudio(){
-  return aSin.next();
-}
-
-void loop(){
-  audioHook();
-}
+/*
+0
+78
+158
+237
+318 
+396 404
+471 477
+548 552
+627 636
+709 715
+788 792
+867 868
+943 943
+*/
