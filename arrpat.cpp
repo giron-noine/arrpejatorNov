f425d49b80bb7f8203f196ad4faa76646046366d
giron-noine <42906002+giron-noine@users.noreply.github.com>
2018-11-22 19:49:15 +0900

    Add files via upload

---
+++ arrpat.cpp
@@ -0,0 +1,303 @@
+#include "arrpat.h"
+#include "arduino.h"
+//#include <MozziGuts.h>
+#include <mozzi_rand.h>
+
+/*
+int scaleMap[6][7] = {
+  {60, 62, 64, 65, 67, 69, 71}, //major
+  {60, 62, 63, 65, 67, 69, 70}, //dorian
+  {60, 62, 64, 67, 69, 72, 74}, //penta
+  {60, 63, 65, 67, 70, 72, 75}, //minor penta 
+  {60, 62, 63, 65, 67, 68, 70}, //minor
+  {60, 61, 64, 65, 67, 68, 71} //gypsy
+};
+
+int scaleMap[6][30] = {
+  {38, 40, 41, 43, 45, 47, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88}, //major
+  {38, 39, 41, 43, 45, 46, 48, 50, 51, 53, 55, 57, 58, 60, 62, 63, 65, 67, 69, 70, 72, 74, 75, 77, 79, 81, 82, 84, 86, 87}, //dorian
+  {26, 28, 31, 33, 36, 38, 40, 43, 45, 48, 50, 52, 55, 57, 60, 62, 64, 67, 69, 72, 74, 76, 79, 81, 84, 86, 88, 91, 93, 96}, //penta
+  {27, 29, 31, 34, 36, 39, 41, 43, 46, 48, 51, 53, 55, 58, 60, 63, 65, 67, 70, 72, 75, 77, 79, 82, 84, 87, 89, 91, 94, 96}, //minor penta
+  {60, 62, 63, 65, 67, 68, 70}, //minor
+  {60, 61, 64, 65, 67, 68, 71} //gypsy
+};
+*/
+
+//int noteNum[13] = {60,61,62,63,64,65,66,67,68,69,70,71,72};
+
+int noteNum;
+
+int scaleMap[6][16] = {
+	{0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 23, 24, 26, 28}, //major
+	{0, 2, 3, 5, 7, 9, 10, 12, 14, 15, 17, 19, 21, 22, 24, 26}, //dorian
+	{0, 2, 4, 7, 9, 12, 14, 16, 19, 21, 24, 26, 28, 31, 33, 36}, //penta
+	{0, 2, 3, 5, 7, 8, 10, 12, 14, 15, 17, 19, 20, 22, 24, 26}, //minor
+	{0, 2, 3, 5, 7, 9, 11, 12, 14, 15, 17, 19, 21, 23, 24, 26}, //melo minor
+	{0, 1, 4, 5, 7, 8, 11, 12, 13, 16, 17, 19, 20, 24, 24, 25}, //gypsy
+};
+
+int noiPatt(int patt, int scale, int note, int steps){
+
+ 	if(patt == 0){ //patt1 is step up
+		noteNum = note + scaleMap[scale][steps] + 60; //60 is 0 to midi note C
+		return noteNum;
+	
+	}else if(patt == 1){ //patt2 is step down
+		switch(steps){
+			case 0:
+				noteNum = note + scaleMap[scale][15] + 60;
+				break;
+			case 1:
+				noteNum = note + scaleMap[scale][14] + 60;
+				break;
+			case 2:
+				noteNum = note + scaleMap[scale][13] + 60;
+				break;
+			case 3:
+				noteNum = note + scaleMap[scale][12] + 60;
+				break;
+			case 4:
+				noteNum = note + scaleMap[scale][11] + 60;
+				break;
+			case 5:
+				noteNum = note + scaleMap[scale][10] + 60;
+				break;
+			case 6:
+				noteNum = note + scaleMap[scale][9] + 60;
+				break;
+      case 7:
+        noteNum = note + scaleMap[scale][8] + 60;
+        break;
+      case 8:
+        noteNum = note + scaleMap[scale][7] + 60;
+        break;
+      case 9:
+        noteNum = note + scaleMap[scale][6] + 60;
+        break;
+      case 10:
+        noteNum = note + scaleMap[scale][5] + 60;
+        break;
+      case 11:
+        noteNum = note + scaleMap[scale][4] + 60;
+        break;
+      case 12:
+        noteNum = note + scaleMap[scale][3] + 60;
+        break;
+      case 13:
+        noteNum = note + scaleMap[scale][2] + 60;
+        break;
+      case 14:
+        noteNum = note + scaleMap[scale][1] + 60;
+        break;
+      case 15:
+        noteNum = note + scaleMap[scale][0] + 60;
+        break;
+		}
+		return noteNum;
+
+	}else if(patt == 2){ //patt3 is skip up
+		switch(steps){
+			case 0:
+				noteNum = note + scaleMap[scale][0] + 60;
+				break;
+			case 1:
+				noteNum = note + scaleMap[scale][3] + 60;
+				break;
+			case 2:
+				noteNum = note + scaleMap[scale][1] + 60;
+				break;
+			case 3:
+				noteNum = note + scaleMap[scale][4] + 60;
+				break;
+			case 4:
+				noteNum = note + scaleMap[scale][2] + 60;
+				break;
+			case 5:
+				noteNum = note + scaleMap[scale][5] + 60;
+				break;
+			case 6:
+				noteNum = note + scaleMap[scale][3] + 60;
+				break;
+      case 7:
+        noteNum = note + scaleMap[scale][6] + 60;
+        break;
+      case 8:
+        noteNum = note + scaleMap[scale][4] + 60;
+        break;
+      case 9:
+        noteNum = note + scaleMap[scale][7] + 60;
+        break;
+      case 10:
+        noteNum = note + scaleMap[scale][5] + 60;
+        break;
+      case 11:
+        noteNum = note + scaleMap[scale][8] + 60;
+        break;
+      case 12:
+        noteNum = note + scaleMap[scale][6] + 60;
+        break;
+      case 13:
+        noteNum = note + scaleMap[scale][9] + 60;
+        break;
+      case 14:
+        noteNum = note + scaleMap[scale][7] + 60;
+        break;
+      case 15:
+        noteNum = note + scaleMap[scale][10] + 60;
+        break;
+		}
+		return noteNum;
+	}else if(patt == 3){ //patt3 is skip center
+		switch(steps){
+			case 0:
+				noteNum = note + scaleMap[scale][0] + 60;
+				break;
+			case 1:
+				noteNum = note + scaleMap[scale][15] + 60;
+				break;
+			case 2:
+				noteNum = note + scaleMap[scale][1] + 60;
+				break;
+			case 3:
+				noteNum = note + scaleMap[scale][14] + 60;
+				break;
+			case 4:
+				noteNum = note + scaleMap[scale][3] + 60;
+				break;
+			case 5:
+				noteNum = note + scaleMap[scale][13] + 60;
+				break;
+			case 6:
+				noteNum = note + scaleMap[scale][4] + 60;
+				break;
+      case 7:
+        noteNum = note + scaleMap[scale][12] + 60;
+        break;
+      case 8:
+        noteNum = note + scaleMap[scale][5] + 60;
+        break;
+      case 9:
+        noteNum = note + scaleMap[scale][11] + 60;
+        break;
+      case 10:
+        noteNum = note + scaleMap[scale][6] + 60;
+        break;
+      case 11:
+        noteNum = note + scaleMap[scale][10] + 60;
+        break;
+      case 12:
+        noteNum = note + scaleMap[scale][7] + 60;
+        break;
+      case 13:
+        noteNum = note + scaleMap[scale][9] + 60;
+        break;
+      case 14:
+        noteNum = note + scaleMap[scale][8] + 60;
+        break;
+      case 15:
+        noteNum = note + scaleMap[scale][9] + 60;
+        break;
+		}
+		return noteNum;
+	}
+	else if(patt == 4){ //patt4 is random
+			switch(steps){
+			case 0:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+			case 1:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+			case 2:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+			case 3:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+			case 4:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+			case 5:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+			case 6:
+				noteNum = note + scaleMap[scale][rand(16)] + 60;
+				break;
+      case 7:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 8:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 9:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 10:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 11:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 12:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 13:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 14:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+      case 15:
+        noteNum = note + scaleMap[scale][rand(16)] + 60;
+        break;
+		}
+		return noteNum; 
+
+  } 
+}
+
+int pushkey(int pushSwitch){
+  if(pushSwitch < 50){
+    return 0; //0
+  }else if(pushSwitch < 100){
+    return 1; //78
+  }else if(pushSwitch < 200){
+    return 2; //158
+  }else if(pushSwitch < 300){
+    return 3; //237
+  }else if(pushSwitch < 350){
+    return 4; //318
+  }else if(pushSwitch < 420){
+    return 5; //404
+  }else if(pushSwitch < 500){
+    return 6; //477
+  }else if(pushSwitch < 600){
+  return 7; //552
+  }else if(pushSwitch < 700){
+  return 8; //636
+  }else if(pushSwitch < 740){
+    return 9; //715
+  }else if(pushSwitch < 840){
+    return 10; //797
+  }else if(pushSwitch < 900){
+    return 11; //871
+  }else if(pushSwitch < 1000){
+    return 12; //947
+  }
+}
+
+/*
+0
+78
+158
+237
+318 
+396 404
+471 477
+548 552
+627 636
+709 715
+788 792
+867 868
+943 943
+*/
--- sketch_nov01a.ino
+++ sketch_nov01a.ino
@@ -1,6 +1,8 @@
-extern "C" {
-  #include "arrpat.h"
-}
+
+
+#include "arrpat.h"
+
+#include <Rotary.h>
 #include <MozziGuts.h>
 #include <Oscil.h>
 #include <mozzi_rand.h>
@@ -11,6 +13,8 @@ extern "C" {
 #define CONTROL_RATE 64
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
 EventDelay kDelay;
+Rotary rotary = Rotary(4, 5);
+
 
 int const nob[3] = {A0,A1,A2};
 
@@ -18,8 +22,8 @@ int const nob[3] = {A0,A1,A2};
 #define switch2 2 //arrp ON
 #define looptop 12
 
-#define octUP 4
-#define octDOWN 5
+//#define octUP 4
+//#define octDOWN 5
 int octv = 0;
 
 int valNob[2][3]; //ページ番号,ノブ番号
@@ -48,8 +52,8 @@ void setup()
   pinMode(switch1, INPUT);
   pinMode(switch2, INPUT);
   pinMode(looptop, OUTPUT);
-  pinMode(octUP, INPUT);
-  pinMode(octDOWN, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
   startMozzi(CONTROL_RATE);
   randSeed();
   kDelay.start(500);
@@ -61,7 +65,10 @@ void setup()
 //---------updateControl start
 void updateControl(){
 
-  tmp_bpm = map(mozziAnalogRead(A4),0,1023,0,500);
+  tmp_bpm = map(mozziAnalogRead(A4),0,1023,0,5000);
+  stepNum = map(valNob[0][0],0,1023,0,15);
+  
+
   
 	if(digitalRead(switch1) == LOW){
     	pageState0 = 1;
@@ -90,14 +97,25 @@ void updateControl(){
   }
   
 //octave control
-  if((digitalRead(5) == 1)&&(octv<3)){
+
+unsigned char result = rotary.process();
+if((result == DIR_CW)&&(octv<3)){
+  octv++;
+  keyshift = octv*12;
+}else if((result == DIR_CCW)&&(octv>-3)){
+  octv--;
+  keyshift = octv*12;
+}
+
+/*
+if((digitalRead(7) == 1)&&(octv<3)){
     octv++;
     keyshift = octv*12;
   }
-  if((digitalRead(4) == 1)&&(octv>-3)){
+  if((digitalRead(8) == 1)&&(octv>-3)){
     octv--;
     keyshift = octv*12;
-  }
+  } */
   
 //push da oscillate
   if(digitalRead(switch2) == LOW){
@@ -105,58 +123,115 @@ void updateControl(){
       playNote = pushkey(mozziAnalogRead(A5)) + 60 + keyshift;
   		aSin.setFreq(mtof(playNote));
     }else{
-    	aSin.setFreq(0);
+    	aSin.setFreq(0.f);
     }
   }else if((digitalRead(switch2) == HIGH)&&(mozziAnalogRead(A5) != 1023)){ //Arrp mode:sometime make liblary
     if(kDelay.ready()){
+     if(bang < stepNum){        
       switch(bang){
-        case 0:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        case 0: //int noiPatt(int patt, int scale, int note, int steps)
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, HIGH);
         bang++;
         break;
 
         case 1:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 2:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 3:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 4:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 5:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 6:
-        aSin.setFreq(mtof(noiPatt(map((valNob[0][0]), 0, 1023, 0, 3), map((valNob[0][1]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang))+ keyshift);
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
-        bang = 0;
+        bang++;
+        break;
+
+        case 7:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 8:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 9:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 10:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 11:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 12:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 13:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 14:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
+        break;
+
+        case 15:
+        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        digitalWrite(12, LOW);
+        bang++;
         break;
       }
-      
+     kDelay.start(tmp_bpm); 
+    }else{
+      bang = 0;
+      }  
     }
-    kDelay.start(tmp_bpm);
-  }else{
-    aSin.setFreq(0);
+   }else{
+    aSin.setFreq(0.f);
   }
 
 /* 
