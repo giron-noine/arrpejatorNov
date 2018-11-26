1cb0229e0ebef3bc6606997af2715c545742f353
giron-noine <42906002+giron-noine@users.noreply.github.com>
2018-11-26 18:32:01 +0900

    Add files via upload

--- sketch_nov01a.ino
+++ sketch_nov01a.ino
@@ -14,7 +14,10 @@
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
 EventDelay kDelay;
 Ead kEnvelope(CONTROL_RATE);
-Rotary rotary = Rotary(4, 5);
+
+Rotary rotary1 = Rotary(0, 1);
+Rotary rotary2 = Rotary(3, 4);
+Rotary rotary3 = Rotary(5, 6);
 
 
 int const nob[3] = {A0,A1,A2};
@@ -33,18 +36,17 @@ int pageState0 = 0;
 int pageState1 = 0;
 
 int stepNum = 16;
-int pattern = 1;
+int pattern = 0;
 int tmp_bpm = 250;
 
-int scaleNum;
-int dechune;
+int scaleNum = 0;
 int keyshift = 0;
 
 int bang = 0;
 //envelope
 int gain;
-unsigned int attack = 100;
-unsigned int decay = 10;
+unsigned int attack = 10;
+unsigned int decay = 100;
 
 int pushkey(int);
 
@@ -56,12 +58,10 @@ void setup()
   pinMode(switch1, INPUT);
   pinMode(switch2, INPUT);
   pinMode(looptop, OUTPUT);
-  pinMode(7, INPUT);
-  pinMode(8, INPUT);
   startMozzi(CONTROL_RATE);
   randSeed();
   kDelay.start(500);
-  //Serial.begin(9600);
+  
 }
 //---------setup end
 
@@ -72,14 +72,51 @@ void updateControl(){
   tmp_bpm = map(mozziAnalogRead(A4),0,1023,0,5000);
   stepNum = map(valNob[0][0],0,1023,0,15);
   
-
-  
 	if(digitalRead(switch1) == LOW){
     	pageState0 = 1;
   	}else{
     	pageState1 = 1;
   	}
 
+//page1 prossece
+	if(pageState0 == 1){
+		unsigned char result1 = rotary1.process(); //pattern select
+			if((result1 == DIR_CW)&&(pattern<5)){
+ 				pattern++;
+  			}else if((result1 == DIR_CCW)&&(pattern>-1)){
+  				pattern--;
+  			}
+
+		unsigned char result2 = rotary2.process(); //scale select
+			if((result2 == DIR_CW)&&(scaleNum<6)){
+ 				scaleNum++;
+  			}else if((result2 == DIR_CCW)&&(scaleNum>-1)){
+  				scaleNum--;
+  			}
+		pageState0 = 0;
+	}
+
+//page2 prossece
+	if(pageState1 == 1){
+		unsigned char result1 = rotary1.process(); //attack select
+			if((result1 == DIR_CW)&&(attack<2000)){
+ 				attack++;
+  			}else if((result1 == DIR_CCW)&&(attack>0)){
+  				attack--;
+  			}
+
+		unsigned char result2 = rotary2.process(); //decay select
+			if((result2 == DIR_CW)&&(decay<2000)){
+ 				decay++;
+  			}else if((result2 == DIR_CCW)&&(decay>0)){
+  				decay--;
+  			}
+		pageState1 = 0;
+	}
+
+
+
+/*
 //page1 prossece
   if(pageState0 == 1){
     for(int i=0; i<3; i++){
@@ -99,17 +136,8 @@ void updateControl(){
     }
     pageState1 = 0;
   }
-  
-//octave control
+*/  
 
-unsigned char result = rotary.process();
-if((result == DIR_CW)&&(octv<3)){
-  octv++;
-  keyshift = octv*12;
-}else if((result == DIR_CCW)&&(octv>-3)){
-  octv--;
-  keyshift = octv*12;
-}
 
 /*
 if((digitalRead(7) == 1)&&(octv<3)){
@@ -121,110 +149,116 @@ if((digitalRead(7) == 1)&&(octv<3)){
     keyshift = octv*12;
   } */
   
+
+
+
 //push da oscillate
   if(digitalRead(switch2) == LOW){
     if(mozziAnalogRead(A5) != 1023){
       playNote = pushkey(mozziAnalogRead(A5)) + 60 + keyshift;
   		aSin.setFreq(mtof(playNote));
+		kEnvelope.start(attack,decay); //エンベロープスタート
     }else{
-    	aSin.setFreq(0.f);
+		kDelay.start(100); //トリガー
+		gain = (int) kEnvelope.next(); 
+    	//aSin.setFreq(0.f);
     }
   }else if((digitalRead(switch2) == HIGH)&&(mozziAnalogRead(A5) != 1023)){ //Arrp mode:sometime make liblary
     if(kDelay.ready()){
      if(bang < stepNum){        
       switch(bang){
         case 0: //int noiPatt(int patt, int scale, int note, int steps)
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, HIGH);
         bang++;
         break;
 
         case 1:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 2:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 3:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 4:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 5:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 6:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 7:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 8:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 9:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 10:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 11:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 12:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 13:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 14:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
 
         case 15:
-        aSin.setFreq(mtof(keyshift + noiPatt(map((valNob[0][1]), 0, 1023, 0, 5), map((valNob[0][2]), 0, 1023, 0, 5), pushkey(mozziAnalogRead(A5)), bang)));
+        aSin.setFreq(mtof(keyshift + noiPatt(pattern, scaleNum, pushkey(mozziAnalogRead(A5)), bang)));
         digitalWrite(12, LOW);
         bang++;
         break;
@@ -260,7 +294,7 @@ if((digitalRead(7) == 1)&&(octv<3)){
 
 //---------updateAudio start
 int updateAudio(){
-	return aSin.next();
+	return (gain*aSin.next())>>8;
 }
 //---------updateAudio end
 
